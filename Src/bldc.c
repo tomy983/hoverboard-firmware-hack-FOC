00644e0ffb34dcc1d2ce535eeebbd9542aa527b3
tomy983 <tomy983@gmail.com>
2022-04-12 11:12:28 +0200

    Update bldc.c

--- Src/bldc.c
+++ Src/bldc.c
@@ -81,6 +81,23 @@ static int16_t offsetdcr    = 2000;
 int16_t        batVoltage       = (400 * BAT_CELLS * BAT_CALIB_ADC) / BAT_CALIB_REAL_VOLTAGE;
 static int32_t batVoltageFixdt  = (400 * BAT_CELLS * BAT_CALIB_ADC) / BAT_CALIB_REAL_VOLTAGE << 16;  // Fixed-point filter output initialized at 400 V*100/cell = 4 V/cell converted to fixed-point
 
+int16_t odom_l = 0;
+int16_t odom_r = 0;
+
+static uint16_t wp_l_vorher = 0;
+static uint16_t wp_r_vorher = 0;
+
+int16_t modulo(int16_t m, int16_t rest_classes){
+  return (((m % rest_classes) + rest_classes) %rest_classes);
+}
+
+int16_t up_or_down(int16_t vorher, int16_t nachher){
+  uint16_t up_down[6] = {0,-1,-2,0,2,1};
+  //uint16_t mod_diff =  (((vorher - nachher) % 6) + 6) % 6;
+  
+  return up_down[modulo(vorher-nachher, 6)];
+}
+
 // =================================
 // DMA interrupt frequency =~ 16 kHz
 // =================================
@@ -199,7 +216,12 @@ void DMA1_Channel1_IRQHandler(void) {
   // errCodeLeft  = rtY_Left.z_errCode;
   // motSpeedLeft = rtY_Left.n_mot;
   // motAngleLeft = rtY_Left.a_elecAngle;
-
+    uint8_t encoding = (uint8_t)((hall_ul<<2) + (hall_vl<<1) + hall_wl);
+    int wheel_pos = rtConstP.vec_hallToPos_Value[encoding];
+  
+    odom_l = modulo(odom_l + up_or_down(wp_l_vorher, wheel_pos), 9000);
+    wp_l_vorher = wheel_pos;
+  
     /* Apply commands */
     LEFT_TIM->LEFT_TIM_U    = (uint16_t)CLAMP(ul + pwm_res / 2, pwm_margin, pwm_res-pwm_margin);
     LEFT_TIM->LEFT_TIM_V    = (uint16_t)CLAMP(vl + pwm_res / 2, pwm_margin, pwm_res-pwm_margin);
@@ -237,6 +259,11 @@ void DMA1_Channel1_IRQHandler(void) {
  // errCodeRight  = rtY_Right.z_errCode;
  // motSpeedRight = rtY_Right.n_mot;
  // motAngleRight = rtY_Right.a_elecAngle;
+
+    encoding = (uint8_t)((hall_ur<<2) + (hall_vr<<1) + hall_wr);
+    wheel_pos = rtConstP.vec_hallToPos_Value[encoding];
+    odom_r = modulo(odom_r - up_or_down(wp_r_vorher, wheel_pos), 9000);
+    wp_r_vorher = wheel_pos;
 
     /* Apply commands */
     RIGHT_TIM->RIGHT_TIM_U  = (uint16_t)CLAMP(ur + pwm_res / 2, pwm_margin, pwm_res-pwm_margin);
